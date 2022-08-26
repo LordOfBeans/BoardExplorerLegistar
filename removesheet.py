b89deb7c6f1ec7021999b138590bb2231ea99c1c
Sean Lord <sean13lord@gmail.com>
2022-08-26 16:33:42 -0400

    Changed Google API Auth
    Authorization has this thing where it throws an error after a week for testing users so I kind of handled that error.

--- removesheet.py
+++ removesheet.py
@@ -11,6 +11,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import google.auth.exceptions
 
 # If true, changes row background color to light gray if there's an x in column A
 # Only works for columns A to Z and rows 2 to 1000
@@ -22,14 +23,12 @@ SCOPES = ['https://www.googleapis.com/auth/drive.file']
 
 # From Google's Python Quickstart for Sheets API
 creds = None
+
 if os.path.exists('token.json'):
 	creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 if not creds or not creds.valid:
-	if creds and creds.expired and creds.refresh_token:
-		creds.refresh(Request())
-	else:
-		flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-		creds = flow.run_local_server(port=0)
+	flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+	creds = flow.run_local_server(port=0)
 	with open('token.json', 'w') as token:
 		token.write(creds.to_json())
 try:
@@ -37,6 +36,17 @@ try:
 	sheetsAPI = service.spreadsheets()
 except HttpError as e:
 	print(e)
+
+def newToken():
+	flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+	creds = flow.run_local_server(port=0)
+	with open('token.json', 'w') as token:
+		token.write(creds.to_json())
+	try:
+		service = build('sheets', 'v4', credentials=creds)
+		sheetsAPI = service.spreadsheets()
+	except HttpError as e:
+		print(e)
 
 # Creates a spreadsheet and returns a new instance of RemoveSheet
 # column_names parameter takes a list of strings
@@ -154,7 +164,11 @@ def createSpreadsheet(title, column_names):
 def getSheet():
 	with open('spreadsheetId.txt', 'r') as id_file:
 		id = id_file.read()
-	get_new = sheetsAPI.values().get(spreadsheetId = id, range = 'New').execute()
+	try:
+		get_new = sheetsAPI.values().get(spreadsheetId = id, range = 'New').execute()
+	except google.auth.exceptions.RefreshError:
+		newToken()
+		get_new = sheetsAPI.values().get(spreadsheetId = id, range = 'New').execute()
 	get_remove = sheetsAPI.values().get(spreadsheetId = id, range = 'Removed').execute()
 	new_values = get_new['values'][1:]
 	remove_values = get_remove['values'][1:]
